c7200eab6ff098b7244bd03f47afb1116299c12a
Flutter <Flutterr>
2026-09-10 21:27:44 +0300

    Initial commit

---
+++ src/main/java/com/amigoscode/Main.java
@@ -0,0 +1,41 @@
+package com.amigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+
+// Java Objects to JSON Objects Example
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        GreetResponse response = new GreetResponse("Hello",
+                List.of("Java", "Golang", "Javascript"),
+                new Person("Alex", 28, 30_000),
+                new Number(List.of(2,5,7,3)
+                )
+        );
+        return response;
+    }
+
+    record Person(String name, int age, double savings){
+
+    }
+
+    record Number(List<Integer> numbers){
+
+    }
+
+    record GreetResponse(String v_greet,
+                         List<String> favProgrammingLanguages,
+                         Person person,
+                         Number number){}
+}
