513ae871b147ad91888e847f3146eec351740cbe
Flutter <Flutterr>
2026-09-11 18:13:22 +0300

    CustomerRepository

--- src/main/java/com/amigoscode/CustomerRepository.java
+++ src/main/java/com/amigoscode/CustomerRepository.java
@@ -2,7 +2,7 @@ package com.amigoscode;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// JpaRepository Contains Table Name & Variable Type of the ID
+// JpaRepository Contains Table Name and Variable Type of the ID
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 
 }
