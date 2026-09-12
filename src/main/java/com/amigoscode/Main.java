0f5953d297f6d0eee7e4304d3fffda0b9ba62a76
Flutter <Flutterr>
2026-09-12 14:00:20 +0300

    Customer APIs

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -1,23 +1,78 @@
 package com.amigoscode;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
 
-// new comment test
+    // For Reading Customers API
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
+
+    }
+
+    record UpdateCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
+
+    }
+
+    // For Creating Customers API
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+
+    }
+
+    // For Deleting Customers API
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    // For Editing the Existing Customers API
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id,
+                               @RequestBody UpdateCustomerRequest request){
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("Customer with id " + id + " not found"));
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
 
 // Java Objects to JSON Objects Example
-    @GetMapping("/greet")
+    /* @GetMapping("/greet")
     public GreetResponse greet(){
         GreetResponse response = new GreetResponse("Hello",
                 List.of("Java", "Golang", "Javascript"),
@@ -39,5 +94,7 @@ public class Main {
     record GreetResponse(String v_greet,
                          List<String> favProgrammingLanguages,
                          Person person,
-                         Number number){}
+                         Number number){} */
+
+
 }
